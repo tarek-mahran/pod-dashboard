343d48e6ba1e7ab6deeb09b5e78bc2c6c31b6618
Tarek Mahran <tarek@example.com>
2025-10-25 11:40:05 +0000

    Fix base path to /pod-dashboard/

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/codespaces-blank/', // Change this to your GitHub repo name
+  base: '/pod-dashboard/',
   server: {
     port: 3000,
     open: true
